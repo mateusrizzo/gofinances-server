8ef244dcf5f5774a171026d9339055c002a61680
Mateus Rizzo <mateusrizzo13@gmail.com>
2020-10-07 16:09:55 -0300

    Challenge completed

--- src/routes/transactions.routes.ts
+++ src/routes/transactions.routes.ts
@@ -5,9 +5,10 @@ import multer from 'multer';
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import CreateTransactionService from '../services/CreateTransactionService';
 import DeleteTransactionService from '../services/DeleteTransactionService';
-// import ImportTransactionsService from '../services/ImportTransactionsService';
+import ImportTransactionsService from '../services/ImportTransactionsService';
 
 import uploadConfig from '../config/upload';
+
 
 const upload = multer(uploadConfig);
 
@@ -50,7 +51,12 @@ transactionsRouter.delete('/:id', async (request, response) => {
 });
 
 transactionsRouter.post('/import', upload.single('file'), async (request, response) => {
-  // TODO
+  const importTransactions = new ImportTransactionsService();
+
+  const transactions = await importTransactions.execute(request.file.path);
+
+  return response.json(transactions);
+
 });
 
 export default transactionsRouter;
